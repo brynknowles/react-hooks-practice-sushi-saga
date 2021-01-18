0e028a686d7cf8ba1bb4a53ccc3a840e1ecb9cd8
brynknowles <bryn.knowles@gmail.com>
2021-01-17 21:59:44 -0500

    first 2 deliverables done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
+// console.log(API)
 
 function App() {
+  const [sushis, setSushis] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then((response) => response.json())
+    // .then(data => console.log(data))
+    .then(setSushis)
+  }, [])
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={sushis}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,19 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis }) {
+  console.log("Props in SushiContainer: ", sushis)
+
+  const sushisToDisplay = sushis.map((sushi) => {
+    return <Sushi key={sushi.id} name={sushi.name} image={sushi.img_url} price={sushi.price} />
+  }).slice(0, 4)
+
+  console.log("sushis to display", sushisToDisplay)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushisToDisplay}
       <MoreButton />
     </div>
   );
